3c50b94c777dfd4f0038b7e71951c74fc5d48502
Mychael M <mychaelbmenges@gmail.com>
2020-10-18 18:21:12 -0600

    creted store and added Provider to App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { carReducer } from "./reducers";
+import { createStore } from "redux";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { Provider } from 'react-redux';
+
+const store = createStore(carReducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
